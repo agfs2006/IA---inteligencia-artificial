a866701d7b1baf8d2944508796c1b1eaec63d38e
Ana Gabriela <119681551+agfs2006@users.noreply.github.com>
2024-08-30 11:14:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -6,67 +7,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Na nova era de técnologias a IA se faz presente em nosso cotidiano, ela esta presente em vídeos, audios e imagens, e o que você acha sobre esse novo avanço tecnológico?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: "Algumas pessoas têm preocupações sobre questões éticas, privacidade e a possibilidade de desemprego, por isso temem o que a IA pode fazer. "
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: "Alguns veem como uma ferramenta revolucionária que pode melhorar vidas e resolver grandes problemas."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, as pessoas costumaram a conversar com a IA, ou o chatGPT, assim podendo tirar duvidas e criar algo novo. Qual dessas questões você pediria ajuda a inteligencia artificial?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Para Informações e Explicações; Ajuda com Estudos e Trabalho; Resolução de Problemas.",
+                afirmacao: "Muitas pessoas buscam informações sobre tópicos diversos, como ciência, história, tecnologia, e questões práticas do dia a dia; Solicitações para ajuda com deveres de casa, pesquisas acadêmicas, escrita de textos e elaboração de projetos são frequentes; Pedem orientações sobre como resolver problemas específicos, desde questões técnicas até dilemas pessoais."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: " Sugestões e Recomendações; Apoio Emocional e Conversas.",
+                afirmacao: "As pessoas também pedem conselhos sobre livros, filmes, música, receitas e até mesmo sugestões para presentes; Algumas pessoas procuram apoio emocional, uma conversa amigável ou uma distração das preocupações diárias."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Como a IA impacta o trabalho do futuro. Nesse debate, como você se posicionaria?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas com esses novos avanços.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA e suas tecnologias."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e robos e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética e produtiva."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "O que esperar do futuro com a Inteligência Artifícial?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "O futuro da IA promete avanços notáveis em assistência médica, educação, automação e inovação em geral. Ao mesmo tempo, a sociedade deve estar atenta aos desafios éticos e garantir que a IA seja uma força positiva para o bem comum.",
+                afirmacao: " As pessoas ainda sentem medo do desconhecido e temem o que pode acontecer."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "A Inteligência Artificial continua a moldar o panorama tecnológico global de maneiras inimagináveis..",
+                afirmacao: "Acelerou o processo de criação de divesas coisas no nosso dia a dia, e tem impulsionado cada vez mais nossa sciedade ao que os antigos diziam 'sociedade futuristica'."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Independentemente do lado em que você esteja, uma coisa não dá para negar: a inteligência artificial já está moldando o mercado de trabalho do futuro. ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Infelizmente tive que me adaptar á essa nova modernidade.",
+                afirmacao: "Infelizmente o medo de perder o controle dessas máquinas me atrapalha o aproveitamento da técnologia."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Esse novo avanço foi bom para a sociedade, então estou feliz em acompanhar.",
+                afirmacao: "Toda essa nova técnologia vai ser bom para nosso crescimento como seres humanos. "
             }
         ]
     },
